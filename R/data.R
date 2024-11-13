a22d588fc1fbb533fc0ca6d730fb1e3217d2d627
Timothy Shen <59162884+timothyshen213@users.noreply.github.com>
2024-11-13 10:41:43 -0800

    pkg data included, vignette added

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Female Mice Liver Expression Data
+#'
+#' @format TODO
+#' 
+"femData"
+
+#' Cleaned Liver Expression Data
+#'
+#' @format TODO
+#' 
+"Liver_Exp"
+
+#' Female Mice Trait Data
+#'
+#' @format TODO
+#' 
+"traitData"
