6b5be6fc078c689aff09c45f5487f92e6d314805
Timothy Shen <59162884+timothyshen213@users.noreply.github.com>
2024-11-14 06:57:51 -0800

    data documentation complete

--- R/data.R
+++ R/data.R
@@ -1,17 +1,44 @@
 #' Female Mice Liver Expression Data
 #'
-#' @format TODO
-#' 
-"femData"
-
-#' Cleaned Liver Expression Data
+#' This dataset includes gene expression levels from liver tissue
+#' of female mice. The data originates from research conducted by 
+#' Ghazalpour et al (\url{https://doi.org/10.1186/1471-2105-9-559}).
+#' It is also widely used as an WGCNA tutorial. However, at this moment,
+#' the documentations for WGCNA no longer exist. See the original
+#' WGCNA paper for more information 
+#' (\url{https://doi.org/10.1186/1471-2105-9-559}).
 #'
-#' @format TODO
-#' 
-"Liver_Exp"
+#' @docType data
+#' @keywords datasets
+#' @name femData
+#' @usage data(femData)
+#' @format A data frame with 66 rows and 3600 columns
+NULL
 
-#' Female Mice Trait Data
+#' Female Mice Clinical Trait Data
 #'
-#' @format TODO
+#' This dataset goes a long with the Female Mice Liver Expression
+#' Data. It contains clinical trait measurements of female mice.
+#' It shares the same observations as Female Mice Liver Expression
+#' Data and provides possible response variables for liver expression.
 #' 
-"traitData"
+#' @docType data
+#' @keywords datasets
+#' @name traitData
+#' @usage data(traitData)
+#' @format A data frame with 38 rows and 361 columns
+NULL
+
+#' Cleaned and Preprocess Female Mice Liver Expression Data
+#' 
+#' Removes outlier genes through WGCNA preprocessing of the Female
+#' Liver Expression Data. Weight (g) variable from Clinical Trait
+#' Data is also included. See Female Mice Liver Expression Vignette
+#' for more information.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Liver_Exp
+#' @usage data(liverExp)
+#' @format A data frame with 66 rows and 3601 columns
+NULL
